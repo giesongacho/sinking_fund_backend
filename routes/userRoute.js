57f61553c4bd4315a56e06f65b8805ebf2202465
Gieson Gacho <giesongacho1@gmail.com>
2025-04-25 19:11:29 +0800

    testing

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const userController = require('../controller/userController')
 const LoginAuth = require('../middleware/loginAuth')
 
-router.post('/create',LoginAuth, userController.CreateUser)
+router.post('/create', userController.CreateUser)
 router.get('/list',LoginAuth, userController.GetUser)
 router.post('/delete/:uuid',LoginAuth, userController.DeleteUser)
 module.exports = router
